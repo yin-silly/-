7193582c1eb3deb4d8833db34def6192116deda0
yin <102793749+yin-silly@users.noreply.github.com>
2023-03-01 20:11:36 +0800

    Add files via upload

---
+++ myPow.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<iomanip>
+
+
+using namespace std;
+
+double quickMul(double x, long long N) {
+    if (N == 0) {
+        return 1.0;
+    }
+    double y = quickMul(x, N / 2);
+    return N % 2 == 0 ? y * y : y * y * x;
+}
+
+double myPow(double x, int n) {
+    long long N = n;
+    return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+
+}
+
+int main() {
+
+	cout <<fixed<<setprecision(5) << myPow(2.00000, 2147483648);
+
+	return 0;
+}
